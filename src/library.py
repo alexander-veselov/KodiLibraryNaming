397c758ec92ba76ad3c62275ee3034226977017d
Oleksandr Veselov <alexander.veselov.dev@gmail.com>
2025-01-24 02:54:43 +0200

    Implement caching

--- src/library.py
+++ src/library.py
@@ -7,12 +7,31 @@ from utils import is_directory, list_directories, input_positive_number
 from naming import rename_tv_show_files
 
 TV_SHOWS_FOLDER = 'TV Shows'
+APP_FOLDER = '.library'
+LOGS_FOLDER = 'logs'
+CACHE_FILE = 'library.cache'
 
 def is_properly_named(name):
     pattern = r'.+ \(\d{4}\)$'
     return bool(re.match(pattern, name))
 
+def read_cache(app_folder_path):
+    cache_path = os.path.join(app_folder_path, CACHE_FILE)
+    if not os.path.exists(cache_path):
+        return set()
+    with open(cache_path, 'r') as f:
+        return set(line.strip() for line in f.readlines())
+
+def update_cache(app_folder_path, tv_shows):
+    cache_path = os.path.join(app_folder_path, CACHE_FILE)
+    with open(cache_path, 'a+') as f:
+        f.writelines(line + '\n' for line in tv_shows)
+
 def main(args):
+    app_folder_path = os.path.join(args.library_path, APP_FOLDER)
+    if not os.path.exists(app_folder_path):
+        os.mkdir(app_folder_path)
+    cache = read_cache(app_folder_path)
     tv_shows_path = os.path.normpath(os.path.join(args.library_path, TV_SHOWS_FOLDER))
     tv_show_paths = list_directories(tv_shows_path)
     invalid_tv_shows = list(filter(lambda x: not is_properly_named(x), tv_show_paths))
@@ -22,18 +41,28 @@ def main(args):
             print(f' - {os.path.basename(invalid_tv_show)}')
         print()
     valid_tv_shows = list(filter(is_properly_named, tv_show_paths))
+    processed_tv_shows = []
     if len(valid_tv_shows) != 0:
         print('[TV shows]')
         for tv_show_path in valid_tv_shows:
-            print(f' - {os.path.basename(tv_show_path)}')
+            tv_show_name = os.path.basename(tv_show_path)
+            if tv_show_name in cache:
+                continue
+            print(f' - {tv_show_name}')
             season = 1
             start_episode = 1
             if args.interactive:
                 season = input_positive_number('Enter season')
                 start_episode = input_positive_number('Enter start episode')
             return_code = rename_tv_show_files(tv_show_path, season, start_episode, args.interactive)
-            if return_code != 0:
-                return return_code
+            if return_code == 0:
+                processed_tv_shows.append(tv_show_name)
+            else:
+                print(f'Renaming {tv_show_name} failed')
+    if len(processed_tv_shows) != 0:
+        update_cache(app_folder_path, processed_tv_shows)
+    else:
+        print('No new TV shows')
     return 0
 
 if __name__ == '__main__':
